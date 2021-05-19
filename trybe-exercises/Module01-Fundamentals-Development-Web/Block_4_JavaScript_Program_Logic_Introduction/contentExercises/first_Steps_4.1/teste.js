150cd7b39df2e0ba99fa2f25ff107452bfb2214f
eaeniko <eaenikolai@gmail.com>
2021-05-19 18:56:26 -0300

    First Content exercise JavaScript

---
+++ trybe-exercises/Module01-Fundamentals-Development-Web/Block_4_JavaScript_Program_Logic_Introduction/contentExercises/first_Steps_4.1/teste.js
@@ -0,0 +1,17 @@
+// Crie uma constante chamada name e atribua a ela o seu nome (Exemplo: Carolina);
+// Crie uma constante chamada birthCity e atribua a ela a sua cidade natal;
+// Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu;
+// Utilize o console.log() para imprimir as constantes e variáveis que você criou;
+// Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+// Altere o valor atribuído à constante birthCity . Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro? 🤔
+
+const name = 'Nikolai';
+const age = 20;
+const birthCity = 'São Paulo';
+
+let birthYear = 1996;
+
+console.log(name);
+console.log(age);
+console.log(birthCity);
+console.log(birthYear);
