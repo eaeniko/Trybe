395bd4b95446664c62ae0d8229d4cfafd6d7ca33
eaeniko <eaenikolai@gmail.com>
2021-05-19 22:56:08 -0300

    [Nikolai] Exercise 1.

---
+++ trybe-exercises/Module01-Fundamentals-Development-Web/Block_4_JavaScript_Program_Logic_Introduction/exercises/exercises.js
@@ -0,0 +1,16 @@
+// 1.
+let a = 5;
+let b = 3;
+
+let adicao = a + b;
+let sub = a - b;
+let multi = a * b;
+let div = a / b;
+let mod = (a % b) == 0;
+
+console.log(adicao);
+console.log(sub);
+console.log(multi);
+console.log(div);
+console.log(mod);
+
---
+++ trybe-exercises/Module01-Fundamentals-Development-Web/Block_4_JavaScript_Program_Logic_Introduction/exercises/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 3;
+
+let adicao = a + b;
+let sub = a - b;
+let multi = a * b;
+let div = a / b;
+let mod = a % b;
