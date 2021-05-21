c54547df8c4f5bcc2917e6d34b8fb88db1024300
eaeniko <eaenikolai@gmail.com>
2021-05-20 22:55:41 -0300

    [Nikolai] Exercise 2

--- trybe-exercises/Module01-Fundamentals-Development-Web/Block_4_JavaScript_Program_Logic_Introduction/4.2_Arrays_For_Loops/contentExercises/Arrays.js
+++ trybe-exercises/Module01-Fundamentals-Development-Web/Block_4_JavaScript_Program_Logic_Introduction/4.2_Arrays_For_Loops/contentExercises/Arrays.js
@@ -1,4 +1,13 @@
+// 1.
+
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu.indexOf('Serviços');
 
 console.log(menuServices);
+
+// 2.
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
---
+++ trybe-exercises/Module01-Fundamentals-Development-Web/Block_4_JavaScript_Program_Logic_Introduction/4.2_Arrays_For_Loops/contentExercises/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
