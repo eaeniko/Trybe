739d1e56ee0aec55d7c8dae24f876faed52c4db6
eaeniko <eaenikolai@gmail.com>
2021-05-21 13:04:26 -0300

    [Nikolai] Exercise 6

--- trybe-exercises/Module01-Fundamentals-Development-Web/Block_4_JavaScript_Program_Logic_Introduction/4.2_Arrays_For_Loops/exercises/exercises.js
+++ trybe-exercises/Module01-Fundamentals-Development-Web/Block_4_JavaScript_Program_Logic_Introduction/4.2_Arrays_For_Loops/exercises/exercises.js
@@ -2,9 +2,8 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let somaTotal = 0;
 
 
-// 5
+// 6
 for (let index = 0; index < numbers.length; index += 1) {
-  //let numbersEach = numbers[index];
   somaTotal = numbers[index] + somaTotal;
   console.log(somaTotal);
 }
@@ -19,14 +18,25 @@ if (media > 20) {
 }
 
 
-let maiorNum = 0;
 for (let index = 0; index < numbers.length; index += 1) {
-  
-  if (numbers[index] > maiorNum) {
-    maiorNum = numbers[index];
+  let impar = 0;
+  if ((numbers[index] % 2) != 0) {
+    impar = numbers[index];
+    console.log(impar);
   } else {
-
-    //console.log(maiorNum);
+    
   }
-} 
-console.log(maiorNum);
+}
+
+// 5.
+// let maiorNum = 0;
+// for (let index = 0; index < numbers.length; index += 1) {
+  
+//   if (numbers[index] > maiorNum) {
+//     maiorNum = numbers[index];
+//   } else {
+
+//     //console.log(maiorNum);
+//   }
+// } 
+// console.log(maiorNum);
