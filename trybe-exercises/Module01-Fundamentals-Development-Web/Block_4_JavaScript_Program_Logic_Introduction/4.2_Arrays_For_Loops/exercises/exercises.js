2f01a62dc115f7cc241cdfbc2d7861141844a649
eaeniko <eaenikolai@gmail.com>
2021-05-21 11:55:57 -0300

    [Nikolai] Exercise 4

--- trybe-exercises/Module01-Fundamentals-Development-Web/Block_4_JavaScript_Program_Logic_Introduction/4.2_Arrays_For_Loops/exercises/exercises.js
+++ trybe-exercises/Module01-Fundamentals-Development-Web/Block_4_JavaScript_Program_Logic_Introduction/4.2_Arrays_For_Loops/exercises/exercises.js
@@ -1,6 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let somaTotal = 0;
-// 3.
+
+
+// 4.
 for (let index = 0; index < numbers.length; index += 1) {
   //let numbersEach = numbers[index];
   somaTotal = numbers[index] + somaTotal;
@@ -9,3 +11,9 @@ for (let index = 0; index < numbers.length; index += 1) {
 
 let media = somaTotal / numbers.length;
 console.log('Media dos numeros ' + media);
+
+if (media > 20) {
+  console.log('Media maior que 20');
+} else {
+  console.log('Media menor que 20');
+}
