895d386e6a4f2b3b808222c6670af422f8f2ac2f
eaeniko <eaenikolai@gmail.com>
2021-05-22 12:15:38 -0300

    [Nikolai] Exercise 2

---
+++ trybe-exercises/Module01-Fundamentals-Development-Web/Block_4_JavaScript_Program_Logic_Introduction/4_3_Array_LoopFor/exercises.js
@@ -0,0 +1,20 @@
+let n = 5;
+let print = "*";
+let line = "";
+
+// // 1
+// for (let index = 0; index < n; index += 1) {
+//   line += print;
+// }
+// for (let index = 0; index < n; index += 1) {
+//   console.log(line);
+// }
+
+// 2
+
+// for (let index = 0; index < n; index += 1) {
+//   line += print;
+//   console.log(line);
+// }
+
+
