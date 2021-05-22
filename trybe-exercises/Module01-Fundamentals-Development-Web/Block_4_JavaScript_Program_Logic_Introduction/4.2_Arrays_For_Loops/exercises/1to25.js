c55036f49ac54704493f896faedb66fc24e78a10
eaeniko <eaenikolai@gmail.com>
2021-05-21 23:50:54 -0300

    [Nikolai] Exercise 7 finished

---
+++ trybe-exercises/Module01-Fundamentals-Development-Web/Block_4_JavaScript_Program_Logic_Introduction/4.2_Arrays_For_Loops/exercises/1to25.js
@@ -0,0 +1,9 @@
+let numbers = [];
+let result = 0;
+for (let index = 1; index <= 25; index += 1) {
+  numbers.push(index);
+
+  result = index / 2;
+  console.log(result);
+}
+//console.log(numbers);
