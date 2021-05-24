65a35ae650e4bc2b21ee2d99019516ede2853e8a
eaeniko <eaenikolai@gmail.com>
2021-05-24 11:34:12 -0300

    [Nikolai] remove Fullname obj prop

--- trybe-exercises/Module01-Fundamentals-Development-Web/Block_4_JavaScript_Program_Logic_Introduction/4_4_Object_Function/contentExercise/firstFunction.js
+++ trybe-exercises/Module01-Fundamentals-Development-Web/Block_4_JavaScript_Program_Logic_Introduction/4_4_Object_Function/contentExercise/firstFunction.js
@@ -1,10 +1,11 @@
 let player = {
   name: 'Marta',
   lastName: 'Silva',
-  fullName: player.name + ' ' + player.lastName,
   age: 34,
   medals: {golden: 2, silver: 3},
-  bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018];
+  bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
 };
 
 console.log('A jogadora' + ' ' + player.name + ' ' + 'tem' + ' ' + player.age + ' ' + 'anos de idade');
+
+console.log('A jogadora possui' + player.medals.golden);
