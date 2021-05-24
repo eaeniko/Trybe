adaffe8e96bdd433cdf52e67097f0e63c18dd93c
eaeniko <eaenikolai@gmail.com>
2021-05-24 15:42:19 -0300

    [Nikolai] Exercise 1

--- trybe-exercises/Module01-Fundamentals-Development-Web/Block_4_JavaScript_Program_Logic_Introduction/4_4_Object_Function/contentExercise/firstFunction.js
+++ trybe-exercises/Module01-Fundamentals-Development-Web/Block_4_JavaScript_Program_Logic_Introduction/4_4_Object_Function/contentExercise/firstFunction.js
@@ -8,4 +8,4 @@ let player = {
 
 console.log('A jogadora' + ' ' + player.name + ' ' + 'tem' + ' ' + player.age + ' ' + 'anos de idade');
 
-console.log('A jogadora possui' + player.medals.golden);
+console.log('A jogadora possui' + ' ' + player.medals.golden + 'medalhas de ouro e' + ' ' + player.medals.silver + ' ' + 'medalhas de prata');
