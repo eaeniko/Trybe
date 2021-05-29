e4273e1ad00fbd4bdb254430345db683106b5251
eaeniko <eaenikolai@gmail.com>
2021-05-28 23:23:19 -0300

    RightSide Taks and footer done.

--- trybe-exercises/Module01-Fundamentals-Development-Web/Block_5_DOOM_selectors/content_exercises/script.js
+++ trybe-exercises/Module01-Fundamentals-Development-Web/Block_5_DOOM_selectors/content_exercises/script.js
@@ -5,5 +5,18 @@ headerBackground.style.backgroundColor = '#2fc18c';
 emergencyTasks = document.querySelectorAll('.emergency-tasks')[0];
 emergencyTasks.style.backgroundColor = '#F7C7DB';
 
-h3EmergencyTaskImportant = document.querySelectorAll('.emergency-tasks h3')[0].style.backgroundColor = 'red';
-h3EmergencyTaskNotImportant = document.querySelectorAll('.emergency-tasks h3')[1].style.backgroundColor = 'red';
+h3EmergencyTaskUrgent = document.querySelectorAll('.emergency-tasks h3')[0].style.backgroundColor = 'red';
+h3EmergencyTaskNotUrgent = document.querySelectorAll('.emergency-tasks h3')[1].style.backgroundColor = 'red';
+
+h3NonUrgencyButImportant = document.querySelectorAll('.no-emergency-tasks h3')[0];
+h3NonUrgencyButImportant.style.backgroundColor = '#2E282A';
+
+h3NonUrgencyNotImportant = document.querySelectorAll('.no-emergency-tasks h3')[1];
+h3NonUrgencyNotImportant.style.backgroundColor = '#2E282A';
+
+notEmergencyTaks = document.querySelectorAll('.no-emergency-tasks')[0];
+notEmergencyTaks.style.backgroundColor = '#FFC914';
+
+
+let footerBackground = document.getElementById('footer-container');
+footerBackground.style.backgroundColor = 'green';
