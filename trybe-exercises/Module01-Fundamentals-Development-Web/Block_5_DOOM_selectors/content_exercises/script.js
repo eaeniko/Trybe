164504db767977c30ae5282181f9a7cca47e05c4
eaeniko <eaenikolai@gmail.com>
2021-05-28 23:14:20 -0300

    LeftSide and Header Colors done

--- trybe-exercises/Module01-Fundamentals-Development-Web/Block_5_DOOM_selectors/content_exercises/script.js
+++ trybe-exercises/Module01-Fundamentals-Development-Web/Block_5_DOOM_selectors/content_exercises/script.js
@@ -1,1 +1,9 @@
 
+let headerBackground = document.getElementById('header-container');
+headerBackground.style.backgroundColor = '#2fc18c';
+
+emergencyTasks = document.querySelectorAll('.emergency-tasks')[0];
+emergencyTasks.style.backgroundColor = '#F7C7DB';
+
+h3EmergencyTaskImportant = document.querySelectorAll('.emergency-tasks h3')[0].style.backgroundColor = 'red';
+h3EmergencyTaskNotImportant = document.querySelectorAll('.emergency-tasks h3')[1].style.backgroundColor = 'red';
