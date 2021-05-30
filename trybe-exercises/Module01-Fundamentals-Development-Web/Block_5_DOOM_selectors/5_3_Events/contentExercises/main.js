300fa18a9384ad098e11a8ed1a16a77bcf65ebc4
eaeniko <eaenikolai@gmail.com>
2021-05-30 12:43:45 -0300

    [Nikolai] exercise 1

--- trybe-exercises/Module01-Fundamentals-Development-Web/Block_5_DOOM_selectors/5_3_Events/contentExercises/main.js
+++ trybe-exercises/Module01-Fundamentals-Development-Web/Block_5_DOOM_selectors/5_3_Events/contentExercises/main.js
@@ -6,18 +6,33 @@ const myWebpage = document.getElementById('mySpotrybefy');
 
 /*
  Copie esse arquivo e edite apenas ele;
- Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
- Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+1Crie uma função que adicione a classe 'tech' ao elemento selecionado;
+Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+
+
+2 Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 com a classe 'tech';
- Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+3Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 redirecione para alguma página;
-1. Que tal redirecionar para seu portifólio?
- Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+. Que tal redirecionar para seu portifólio?
+3 Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 a cor do mesmo;
 
 Segue abaixo um exemplo do uso de event.target:
 */
+
+// function removeTech(event){
+//   event.target.removeEventListener('click', event);
+// }
+
+function classTech(event) {
+  console.log(event.target);
+    event.target.className = 'tech';
+}
+
+divUm.addEventListener('click', classTech);
+divDois.addEventListener('click', classTech);
+divTres.addEventListener('click', classTech);
 
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
