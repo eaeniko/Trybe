2ad73c6719f6dfebca0c4f2023b496fca2381a16
eaeniko <eaenikolai@gmail.com>
2021-06-18 14:37:49 -0300

    exercise 2 - sort with array function and template literal

---
+++ trybe-exercises/Block_7_ES6_&_UnitTest/exercises/exercise2.js
@@ -0,0 +1,6 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+oddsAndEvens.sort((a, b) => a - b);
+
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
