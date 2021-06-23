c2b58a04e700dffe835420b487148196dceda57e
eaeniko <eaenikolai@gmail.com>
2021-06-23 19:57:52 -0300

    exercise 1

---
+++ trybe-exercises/Module01-Fundamentals-Development-Web/Block_8_HOF/8_1_HOF_Introduction/exercises/exercise1.js
@@ -0,0 +1,18 @@
+const person = (name) => {
+  const obj = {
+    nomeCompleto: name,
+    email: `${name.toLowerCase().replace(' ', "_")}@trybe.com`,
+  }
+  return obj;
+};
+
+const newEmployees = (employee) => {
+  const employees = {
+    id1: person('Pedro Guerra'), // Nome: Pedro Guerra  -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: person('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: person('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+console.log(newEmployees(person));
