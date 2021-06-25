b94accc374adb88cf2625dc5ac4b3121e05eadba
eaeniko <eaenikolai@gmail.com>
2021-06-25 19:19:09 -0300

    find ex 2

--- trybe-exercises/Module01-Fundamentals-Development-Web/Block_8_HOF/8_2_ES6_HOF_forEach_Find_Some_Every_Sort/contentExercises/find.js
+++ trybe-exercises/Module01-Fundamentals-Development-Web/Block_8_HOF/8_2_ES6_HOF_forEach_Find_Some_Every_Sort/contentExercises/find.js
@@ -4,3 +4,12 @@ const numbers = [19, 21, 30, 3, 45, 22, 15];
 const findDivisibleBy3And5 = () => {return numbers.find((number) => number % 3 === 0 || number % 5 === 0);}
 
 console.log(findDivisibleBy3And5())
+
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => {
+  return names.find((name) => name.length === 5);
+}
+
+console.log(findNameWithFiveLetters());
