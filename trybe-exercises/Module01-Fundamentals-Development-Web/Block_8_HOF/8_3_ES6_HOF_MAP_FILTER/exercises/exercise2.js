b5083dea96282ed87639e570ec5143492d1166f9
eaeniko <eaenikolai@gmail.com>
2021-06-30 15:15:22 -0300

    exercise 1 map

--- trybe-exercises/Module01-Fundamentals-Development-Web/Block_8_HOF/8_3_ES6_HOF_MAP_FILTER/exercises/exercise2.js
+++ trybe-exercises/Module01-Fundamentals-Development-Web/Block_8_HOF/8_3_ES6_HOF_MAP_FILTER/exercises/exercise2.js
@@ -65,3 +65,35 @@ const books = [
 
 // Adicione o código do exercício aqui:
 
+const expectedResult = [
+  {
+    age: 31,
+    author: 'Isaac Asimov',
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft',
+  },
+  {
+    age: 39,
+    author: 'Stephen King',
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin',
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert',
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien',
+  },
+];
+
+function nameAndAge() {
+  // escreva seu código aqui
+}
+
+assert.deepStrictEqual(nameAndAge(), expectedResult);
