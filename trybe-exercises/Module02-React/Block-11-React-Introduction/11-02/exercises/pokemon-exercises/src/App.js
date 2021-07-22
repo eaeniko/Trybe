57381cc43b0e9e7050205d8da718d635d86923bf
eaeniko <eaenikolai@gmail.com>
2021-07-22 10:28:00 -0300

    Pokedex list with react components

---
+++ trybe-exercises/Module02-React/Block-11-React-Introduction/11-02/exercises/pokemon-exercises/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Pokemons from './Pokemons';
+
+function App() {
+  return (
+    <div className="pokedex">
+      <h1 className="title">Pokemons</h1>
+      <Pokemons />
+    </div>
+  );
+}
+
+export default App;
---
+++ trybe-exercises/Module02-React/Block-11-React-Introduction/11-02/exercises/pokemon-exercises/src/Pokemons.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import pokemons from './data.js'
+
+class Pokemons extends Component {
+
+  render() {
+    return pokemons.map((pokemon) => (
+      <section key={pokemon.id} className="pokemon">
+        <p><span className="font">Name:</span> {pokemon.name}</p>
+        <p><span className="font">Type:</span> {pokemon.type}</p>
+        <p><span className="font">Weight:</span> {pokemon.averageWeight.value} {pokemon.averageWeight.measurementUnit}</p>
+        <img src={pokemon.image} alt= {`Imagem do Pokemon ${pokemon.name}`}/>
+      </section>
+    ))
+  }
+}
+
+export default Pokemons
